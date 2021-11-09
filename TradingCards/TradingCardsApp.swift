e7689bfacb44e4a211a98597e9aa33855319921e
Suzanne <STian@lcs.on.ca>
2021-11-09 11:28:22 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Suzanne Tian on 2021-11-09.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
